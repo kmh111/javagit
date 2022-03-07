cbd0c52a97326059a0433de44f9ccaadfe2b3e68
이승민 <@Seungmin>
2022-03-07 21:09:47 +0900

    java project

---
+++ src/javaproject/Operator_5.java
@@ -0,0 +1,31 @@
+package javaproject;
+
+public class Operator_5 {
+
+	public static void main(String[] args) {
+
+		
+		
+		Object obj1 = new Object();
+		Object obj2 = new Object();
+		
+		
+		
+		System.out.println(obj1==obj2);
+		System.out.println(obj1.equals(obj2));
+		
+		
+		System.out.println();
+		
+		String str1 = new String("±è¹ÎÇÐ");
+		String str2 = new String("±è¹ÎÇÐ");
+		
+		System.out.println(str1==str2);
+		System.out.println(str1.equals(str2));
+		
+		
+		
+		
+	}
+
+}
